f78c00307813979211ca1ed94da66c7aba789121
vuongvu <hoangvuong.v99@gmail.com>
2018-12-11 23:15:08 +0700

    Get 1 match details

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ const { autoScroll } = require('./utils');
 
 const PUPPETEER_LAUNCH_OPTIONS = {
   args: ["--lang=en-US", "--no-sandbox", "--disable-setuid-sandbox"],
-  headless: false,
+  // headless: false,
   // slowMo: 500 
 };
 const PUPPETEER_PAGE_VIEWPORT = { width: 1366, height: 768 };
 const LOAD_MORE_TIME = 3;
 
 const getPageContent = async (pageUrl, page) => {
-  
-
   for (let i = 1; i < LOAD_MORE_TIME; i = i + 1) {
     if (i === 1) {
       await page.goto(pageUrl, { waitUntil: 'networkidle2' });
@@ -28,26 +26,48 @@ const getPageContent = async (pageUrl, page) => {
   return pageContent;
 };
 
-const extractPageContent = async (pageContent) => {
-  const pageElementHandler = cheerio.load(pageContent);
-  const allMatchesContainer = pageElementHandler('.td_block_wrap .td_block_inner .td_module_wrap .td-module-meta-info .td-module-title');
+const extractPageContent = async (content) => {
+  const wrapperContainer = cheerio.load(content);
+  const wrapper = wrapperContainer('.td_block_wrap .td_block_inner .td_module_wrap .td-module-meta-info');
   const matchesData = [];
 
-  allMatchesContainer.each((index, container) => {
+  wrapper.each((index, container) => {
     const element = cheerio.load(container);
-    const elementTitle = element('a');
+    const elementTitle = element('.td-module-title a');
+    const timeTag = element('time');
     matchesData.push({
       index,
       title: elementTitle.attr("title") || elementTitle.text(),
       url: elementTitle.attr("href"),
+      time: timeTag.attr("datetime"),
     });
   });
   return matchesData
 };
 
+const extractPageDetail = async (content) => {
+  const wrapperContainer = cheerio.load(content);
+  const wrapper = wrapperContainer('#acp_wrapper');
+  const matchData = [];
+
+  wrapper.each((index, container) => {
+    const element = cheerio.load(container);
+    const elementTitle = element('.acp_title');
+    const elementIframe = element('.acp_content iframe');
+    matchData.push({
+      title: elementTitle.text(),
+      src: elementIframe.attr("src"),
+    });
+  });
+  return matchData
+};
+
 const getMatchDetails = async (url, page) => {
-  console.log(url);
   await page.goto(url, { waitUnil: 'networkidle2' });
+  const pageDetailContent = await page.content();
+  const matchDetails = await extractPageDetail(pageDetailContent);
+
+  console.log({ matchDetails });
 };
 
 const main = async () => {
@@ -59,6 +79,7 @@ const main = async () => {
   const targetContent = await getPageContent(targetUrl, page);
   const matches = await extractPageContent(targetContent);
   await getMatchDetails(matches[0].url, page);
+  // console.log({ matches });
 
   await browser.close();
 }
